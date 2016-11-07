e7555afd11d39b6aa679053de37925ef0a73c2c9
lm3190 <lm3190@tc.columbia.edu>
2016-11-07 16:28:33 -0500

    Final edits
    pseudo code, column fixes

---
+++ Mid-term 5003/main.js
@@ -0,0 +1,53 @@
+/*function quiz() { <!--quiz question 1 answer is anger-->
+    var angerAnswer = true
+    if (angerAnswer === true) {
+        alert("Correct Answer!";)
+    } else {
+        alert("Incorrect, the emotion is Anger");
+    }
+}
+
+function quiz() { <!--quiz question 2 answer is anger-->
+    var supriseAnswer = true
+    if (supriseAnswer === true) {
+        alert("Correct Answer!";)
+    } else {
+        alert("Incorrect, the emotion is Suprise");
+    }
+}
+
+function quiz() { <!--quiz question 3 answer is disgust-->
+    var disgustAnswer = true
+    if (disgustAnswer === true) {
+        alert("Correct Answer!";)
+    } else {
+        alert("Incorrect, the emotion is Disgust");
+    }
+}
+
+function quiz() { <!--quiz question 4 answer is disgust-->
+    var disgustClintonAnswer = true
+    if (disgustClintonAnswer === true) {
+        alert("Correct Answer!";)
+    } else {
+        alert("Incorrect, the emotion is Disgust");
+    }
+}
+
+function quiz() { <!--quiz question 5 answer is suprise-->
+    var supriseTrumpAnswer = true
+    if (supriseTrumpAnswer === true) {
+        alert("Correct Answer!";)
+    } else {
+        alert("Incorrect, the emotion is Suprise");
+    }
+}
+
+function quiz() { <!--quiz question 6 answer is happy-->
+    var happyAnswer = true
+    if (happyAnswer === true) {
+        alert("Correct Answer!";)
+    } else {
+        alert("Incorrect, the emotion is Happy");
+    }
+} */
