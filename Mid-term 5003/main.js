9b4866608e0e25d6e4fc967af54103919807c111
lm3190 <lm3190@tc.columbia.edu>
2016-11-08 19:57:39 -0500

    Final changes
    added js code and html button fixes

--- Mid-term 5003/main.js
+++ Mid-term 5003/main.js
@@ -1,53 +1,71 @@
-/*function quiz() { <!--quiz question 1 answer is anger-->
-    var angerAnswer = true
-    if (angerAnswer === true) {
-        alert("Correct Answer!";)
-    } else {
-        alert("Incorrect, the emotion is Anger");
-    }
-}
+var question1 = function() { /*question 1 answer is anger*/
+    alert("Incorrect, the answer is Anger");
+};
 
-function quiz() { <!--quiz question 2 answer is anger-->
-    var supriseAnswer = true
-    if (supriseAnswer === true) {
-        alert("Correct Answer!";)
-    } else {
-        alert("Incorrect, the emotion is Suprise");
-    }
-}
+document.getElementById("button1").addEventListener('click', question1);
 
-function quiz() { <!--quiz question 3 answer is disgust-->
-    var disgustAnswer = true
-    if (disgustAnswer === true) {
-        alert("Correct Answer!";)
-    } else {
-        alert("Incorrect, the emotion is Disgust");
-    }
-}
+var question1Anger = function() {
+    alert("Correct");
+};
 
-function quiz() { <!--quiz question 4 answer is disgust-->
-    var disgustClintonAnswer = true
-    if (disgustClintonAnswer === true) {
-        alert("Correct Answer!";)
-    } else {
-        alert("Incorrect, the emotion is Disgust");
-    }
-}
+document.getElementById("button2").addEventListener('click', question1Anger);
 
-function quiz() { <!--quiz question 5 answer is suprise-->
-    var supriseTrumpAnswer = true
-    if (supriseTrumpAnswer === true) {
-        alert("Correct Answer!";)
-    } else {
-        alert("Incorrect, the emotion is Suprise");
-    }
-}
+var question2 = function() { /*question 2 answer is suprise*/
+    alert("Incorrect, the answer is Suprise");
+};
 
-function quiz() { <!--quiz question 6 answer is happy-->
-    var happyAnswer = true
-    if (happyAnswer === true) {
-        alert("Correct Answer!";)
-    } else {
-        alert("Incorrect, the emotion is Happy");
-    }
-} */
+document.getElementById("button3").addEventListener('click', question2);
+
+var question2Suprise = function() {
+    alert("Correct");
+};
+
+document.getElementById("button4").addEventListener('click', question2Suprise);
+
+var question3 = function() { /*question 3 answer is disgust*/
+    alert("Incorrect, the answer is disgust");
+};
+
+document.getElementById("button5").addEventListener('click', question3);
+
+var question3Disgust = function() {
+    alert("Correct");
+};
+
+document.getElementById("button6").addEventListener('click', question3Disgust);
+
+var question4 = function() { /*question 4 answer is disgust*/
+    alert("Incorrect, the answer is disgust");
+};
+
+document.getElementById("button7").addEventListener('click', question4);
+
+var question4Disgust = function() {
+    alert("Correct");
+};
+
+document.getElementById("button8").addEventListener('click', question4Disgust);
+
+var question5 = function() { /*question 5 answer is suprise*/
+    alert("Correct");
+};
+
+document.getElementById("button9").addEventListener('click', question5);
+
+var question5suprise = function() {
+    alert("Incorrect, the answer is suprise");
+};
+
+document.getElementById("button10").addEventListener('click', question5suprise);
+
+var question6 = function() { /*question 6 answer is happy*/
+    alert("Incorrect, the answer is happy");
+};
+
+document.getElementById("button11").addEventListener('click', question6);
+
+var question6happy = function() {
+    alert("Correct");
+};
+
+document.getElementById("button12").addEventListener('click', question6happy);
